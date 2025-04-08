936edbb4e2e8da9f9c3091fe6cb869326e51b83a
Ivan Schaefer <Meleemanhs@gmail.com>
2025-04-08 21:42:09 +0200

    added token for github ci-cd, working

--- modules/cicd/main.tf
+++ modules/cicd/main.tf
@@ -2,14 +2,31 @@ terraform {
   backend "s3" {}
 }
 
+provider "aws" {
+  region = "eu-central-1"
+}
 
+# OIDC provider for GitHub Actions
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+# Assume Role policy document for GitHub Actions
 data "aws_iam_policy_document" "github_actions_assume_role" {
   statement {
     effect = "Allow"
 
     principals {
       type        = "Federated"
-      identifiers = ["arn:aws:iam::${var.aws_account_id}:oidc-provider/token.actions.githubusercontent.com"]
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
 
     actions = ["sts:AssumeRoleWithWebIdentity"]
@@ -21,17 +38,15 @@ data "aws_iam_policy_document" "github_actions_assume_role" {
     }
   }
 }
+
+# IAM Role that GitHub Actions assumes
 resource "aws_iam_role" "github_actions_oidc" {
   name               = "github-actions-oidc-role"
   assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
 }
 
+# Attach ECR push permissions
 resource "aws_iam_role_policy_attachment" "ecr_access" {
   role       = aws_iam_role.github_actions_oidc.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_readonly" {
-  role       = aws_iam_role.github_actions_oidc.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSReadOnlyAccess"
 }
